62d7d09ad52639283d7837be5dec1db99e189131
BMRaed7 <62666504+BenMaghzouz-Raed@users.noreply.github.com>
2022-06-02 20:14:07 +0100

    first commit

---
+++ Pages/footer.php
@@ -0,0 +1,42 @@
+<footer>
+        <div class="columns">
+
+            <div class="logo">
+                <img src="./Images/logo2.svg" alt="" id="logo">
+
+                <div class="social">
+                    <img src="./Images/facebook-logo-64.png" alt="">
+                    <img src="./Images/instagram-logo-64.png" alt="">
+                </div>
+            </div>
+
+            <div class="line"></div>
+            
+            <div class="info">
+                <h3>Contact</h3>
+                <p>48 Farhat Hachad 5190<br/>
+                    Mahdia, Tunisia
+                </p>
+                <p>Fax: +216 73 658 118<br/>
+                    Phone: +216 50 448 714<br/>
+                    Phone: +216 55 091 815
+                </p>
+                <p>Email: contact@GCT-trade.com</p>
+                
+            </div>
+
+            <div class="line"></div>
+
+            <div class="info">
+                <h3>Information</h3>
+                <p>Our Product</p>
+                <p>Why Us</p>
+                <p>Origins</p>
+                <p>Help & FAQ</p>
+            </div>
+
+        </div>
+        <div class="credit">
+            ©2020 All Rights Reserved. This website is made by <span><a href="mailto:bmraed7@gmail.com">BMRaed</a></span>
+        </div>
+    </footer>
